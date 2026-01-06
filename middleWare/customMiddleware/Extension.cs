b0940f9289c01f011e0bfb7ca93bbd37149cb27d
mody4z <modygaber23572357@gmail.com>
2026-01-06 17:25:11 +0200

    middleWare Task

---
+++ middleWare/customMiddleware/Extension.cs
@@ -0,0 +1,14 @@
+﻿namespace middleWare.customMiddleware
+{
+    public static class Extension
+    {
+     public static IApplicationBuilder UseMycustomMiddlewarecs(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MycustomMiddlewarecs>();
+        }
+        public static IApplicationBuilder UseLoginCheck(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LoginCheck>();
+        }
+    }
+}
---
+++ middleWare/customMiddleware/LoginCheck.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace middleWare.customMiddleware
+{
+  
+    public class LoginCheck
+    {
+       
+        private readonly RequestDelegate _next;
+        public LoginCheck(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Path == "/" && context.Request.Method == "POST")
+            {
+                var form = await context.Request.ReadFormAsync();
+
+                var username = form["username"];
+                var password = form["password"];
+
+                if (StringValues.IsNullOrEmpty(username))
+                {
+                    context.Response.StatusCode = 400;
+                    await context.Response.WriteAsync("Invalid input for 'username'");
+                    return;
+                }
+
+                if (  StringValues.IsNullOrEmpty(password))
+                {
+                    context.Response.StatusCode = 400;
+                    await context.Response.WriteAsync("Invalid input for 'password'");
+                    return;                                                                     
+                }
+                 var usernameStr = username.ToString().Trim('"');
+                var passwordStr = password.ToString().Trim('"');
+
+                if (usernameStr == "admin" && passwordStr == "admin@123")
+                {
+                    context.Response.StatusCode = 200;
+                    await context.Response.WriteAsync("Successful login");
+                    return;
+                }
+                else
+                {
+                    context.Response.StatusCode = 401;
+                    await context.Response.WriteAsync("Invalid login");
+                    return;
+                }
+            }
+
+        }
+
+    }
+}
